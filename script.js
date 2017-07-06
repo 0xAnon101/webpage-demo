99b592d3709bbf68cc8e41ccdec24529bafd5509
Raju Gautam <ARMY_BRAT@upes-500047678.DDN.UPES.AC.IN>
2017-07-05 17:01:49 -0700

    add a new js file 1

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("femail").onfocus= function() {changeColor() };
+document.getElementById("fpass").onfocus= function() {changeColor() };
+
+
+
+function changeColor() {
+    var x = document.getElementById("second-content");
+    x.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+document.getElementById("login").onclick = function() { gotoLogin() };
+
